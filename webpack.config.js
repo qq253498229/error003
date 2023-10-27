418f40f7d7a649fa9812b97d68a75f307aee35cf
王斌 <253498229@qq.com>
2023-10-27 13:37:34 +0800

    feat: 配置共享模块

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {share, withModuleFederationPlugin} = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "mfe1": "http://localhost:3000/remoteEntry.js",    
+    // "mfe1": "http://localhost:3000/remoteEntry.js",
   },
 
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+  shared: share({
+    '@angular/core': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    '@angular/common': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    '@angular/common/http': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    '@angular/router': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    '@angular/cdk': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    '@ngxs/store': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    '@ngxs/router-plugin': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    '@ngxs/storage-plugin': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    '@ngxs/form-plugin': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    'ng-zorro-antd': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    'lodash': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    'object-path-immutable': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+    'ngx-auto-unsubscribe-decorator': {singleton: true, strictVersion: true, requiredVersion: 'auto',},
+  }),
+
+  sharedMappings: [],
 
 });
